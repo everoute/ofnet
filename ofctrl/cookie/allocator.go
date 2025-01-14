3f51df497bb07a40b840a0d8ce22c4e5f993cc36
Jiao Zhou <jiao.zhou@smartx.com>
2025-01-14 11:08:55 +0800

    feat: support set flowid range in allocator

--- ofctrl/cookie/allocator.go
+++ ofctrl/cookie/allocator.go
@@ -1,6 +1,7 @@
 package cookie
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -10,7 +11,10 @@ const (
 	BitWidthFlowId          = 64 - BitWidthReserved - BitWidthRoundNum
 	RoundNumMask     uint64 = 0x0000_0000_f000_0000
 	FlowIdMask       uint64 = 0x0000_0000_0fff_ffff
+	InitFlowID       uint64 = 1
 )
+
+var FlowIDExhausted error = fmt.Errorf("flow id has exhausted")
 
 type ID uint64
 
@@ -31,15 +35,18 @@ func (i ID) Round() uint64 {
 }
 
 type Allocator interface {
-	RequestCookie() uint64
+	RequestCookie() (uint64, error)
 	SetFixedMask(uint64)
 }
+
+type AllocatorOption func(*allocator) error
 
 type allocator struct {
 	roundNum   uint64
 	flowID     uint64
 	fixedMask  uint64
 	flowIDLock sync.RWMutex
+	maxFlowID  uint64
 }
 
 // cookie will 'OR' fixed mask
@@ -47,20 +54,62 @@ func (a *allocator) SetFixedMask(mask uint64) {
 	a.fixedMask = mask
 }
 
-func (a *allocator) RequestCookie() uint64 {
+func (a *allocator) RequestCookie() (uint64, error) {
 	a.flowIDLock.Lock()
 	defer a.flowIDLock.Unlock()
 
+	if a.maxFlowID != 0 && a.flowID > a.maxFlowID {
+		return 0, FlowIDExhausted
+	}
 	rawID := newId(a.roundNum, a.flowID).RawId()
 	a.flowID += 1
-	return rawID | a.fixedMask
+	return rawID | a.fixedMask, nil
 }
 
-func NewAllocator(roundNum uint64) Allocator {
+func (a *allocator) setFlowIDRange(start, end uint64) error {
+	if start > end {
+		return fmt.Errorf("param error, start %x can't bigger than end %x", start, end)
+	}
+	maxFlowIDOrigin := uint64(1<<BitWidthFlowId - 1)
+	if start < InitFlowID {
+		return fmt.Errorf("start %x can't small than minFlowID %x", start, InitFlowID)
+	}
+	if end > maxFlowIDOrigin {
+		return fmt.Errorf("end %x can't biggger than maxFlowID %x", end, maxFlowIDOrigin)
+	}
+	a.flowID = start
+	a.maxFlowID = end
+	return nil
+}
+
+func (a *allocator) setDefaultFlowIDRange() {
+	a.flowID = InitFlowID
+	a.maxFlowID = uint64(1<<BitWidthFlowId - 1)
+}
+
+func SetFlowIDRange(start, end uint64) AllocatorOption {
+	return func(a *allocator) error {
+		return a.setFlowIDRange(start, end)
+	}
+}
+
+func SetDefaultFlowIDRange() AllocatorOption {
+	return func(a *allocator) error {
+		a.setDefaultFlowIDRange()
+		return nil
+	}
+}
+
+func NewAllocator(roundNum uint64, options ...AllocatorOption) Allocator {
 	a := &allocator{
 		roundNum:   roundNum,
-		flowID:     1,
+		flowID:     InitFlowID,
 		flowIDLock: sync.RWMutex{},
+	}
+	for _, o := range options {
+		if err := o(a); err != nil {
+			return nil
+		}
 	}
 	return a
 }
--- ofctrl/cookie/allocator_test.go
+++ ofctrl/cookie/allocator_test.go
@@ -7,39 +7,105 @@ import (
 func TestAllocateCookie(t *testing.T) {
 	allocator := NewAllocator(0)
 
-	cookie := allocator.RequestCookie()
+	cookie, err := allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x1 {
 		t.Errorf("request cookie fail, expect 0x1, got %x", cookie)
 	}
 
-	cookie = allocator.RequestCookie()
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x2 {
 		t.Errorf("request cookie fail, expect 0x2, got %x", cookie)
 	}
 
 	allocator.SetFixedMask(0x10)
 
-	cookie = allocator.RequestCookie()
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x13 {
 		t.Errorf("request cookie fail, expect 0x13, got %x", cookie)
 	}
 
 	allocator = NewAllocator(1)
 
-	cookie = allocator.RequestCookie()
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x10000001 {
 		t.Errorf("request cookie fail, expect 0x10000001, got %x", cookie)
 	}
 
-	cookie = allocator.RequestCookie()
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x10000002 {
 		t.Errorf("request cookie fail, expect 0x10000002, got %x", cookie)
 	}
 
 	allocator.SetFixedMask(0x100000000)
 
-	cookie = allocator.RequestCookie()
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
 	if cookie != 0x110000003 {
 		t.Errorf("request cookie fail, expect 0x110000003, got %x", cookie)
+	}
+
+	allocator = NewAllocator(1, SetFlowIDRange(10, 9))
+	if allocator != nil {
+		t.Errorf("expect allocator is nil, but real is not")
+	}
+
+	allocator = NewAllocator(1, SetFlowIDRange(0, 9))
+	if allocator != nil {
+		t.Errorf("expect allocator is nil, but real is not")
+	}
+
+	allocator = NewAllocator(1, SetFlowIDRange(10, 1<<BitWidthFlowId))
+	if allocator != nil {
+		t.Errorf("expect allocator is nil, but real is not")
+	}
+
+	allocator = NewAllocator(1, SetFlowIDRange(10, 11))
+
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
+	if cookie != 0x1000000a {
+		t.Errorf("request cookie fail, expect 0x1000000a , got %x", cookie)
+	}
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
+	if cookie != 0x1000000b {
+		t.Errorf("request cookie fail, expect 0x1000000b, got %x", cookie)
+	}
+	cookie, err = allocator.RequestCookie()
+	if err == nil {
+		t.Errorf("expect allocate cookie failed, but success")
+	}
+
+	allocator = NewAllocator(1, SetDefaultFlowIDRange())
+	if allocator == nil {
+		t.Errorf("new allocator failed, is unexpected")
+	}
+	cookie, err = allocator.RequestCookie()
+	if err != nil {
+		t.Errorf("unexpectd return err %s", err)
+	}
+	if cookie != 0x10000001 {
+		t.Errorf("request cookie fail, expect 0x10000001, got %x", cookie)
 	}
 }
